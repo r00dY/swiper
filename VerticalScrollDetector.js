f80b9ad6f84172e79f8bfafc60a59265f2193816
Mati <mati@ratio.team>
2018-10-25 18:18:11 +0200

    undoing tests

--- VerticalScrollDetector.js
+++ VerticalScrollDetector.js
@@ -3,24 +3,24 @@ let VerticalScrollDetector = new function() {
 	var initScroll;
 	var isScrolling = false;
 
-	// window.addEventListener('touchstart', function(ev) {
-    //
-     //    initScroll = window.pageYOffset || document.documentElement.scrollTop;
-	// });
-    //
-	// window.addEventListener('touchmove', function(ev) {
-	// 	if (!isScrolling) {
-	// 		var scroll = window.pageYOffset || document.documentElement.scrollTop;
-    //
-	// 		if (scroll != initScroll) {
-	// 			isScrolling = true;
-	// 		}
-	// 	}
-	// });
-    //
-	// window.addEventListener("touchend", function(ev) {
-	// 	isScrolling = false;
-	// });
+	window.addEventListener('touchstart', function(ev) {
+
+        initScroll = window.pageYOffset || document.documentElement.scrollTop;
+	});
+
+	window.addEventListener('touchmove', function(ev) {
+		if (!isScrolling) {
+			var scroll = window.pageYOffset || document.documentElement.scrollTop;
+
+			if (scroll != initScroll) {
+				isScrolling = true;
+			}
+		}
+	});
+
+	window.addEventListener("touchend", function(ev) {
+		isScrolling = false;
+	});
 
 	this.isScrolling = function() {
 		return isScrolling;
