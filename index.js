6d80b749aff75a2b35013d7487d85c44207c21f3
Andrzej Dąbrowski <andrzej@ratio.team>
2018-11-30 09:46:19 +0100

    build

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-import SwiperEngine from "./src/native/SwiperEngine";
-
-import TouchSwiper from "./src/native/TouchSwiper";
-
-import SimpleSwiper from "./src/native/SimpleSwiper";
-import FadeSwiper from "./src/native/FadeSwiper";
+import AbstractSlider from "./src/AbstractSlider";
+import SimpleSlider from "./src/SimpleSlider";
 
 import SwiperArrows from './src/components/arrows/SwiperArrows';
 import SwiperPager from './src/components/pager/SwiperPager';
 
-import ReactSimpleSwiper from "./src/react/ReactSimpleSwiper";
-import ReactFadeSwiper from "./src/react/ReactFadeSwiper";
 
 export {
-    SwiperEngine,
-    TouchSwiper,
-    SimpleSwiper,
-    FadeSwiper,
+    AbstractSlider,
+    SimpleSlider,
     SwiperArrows,
-    SwiperPager,
-    ReactSimpleSwiper,
-    ReactFadeSwiper
+    SwiperPager
 };
 
