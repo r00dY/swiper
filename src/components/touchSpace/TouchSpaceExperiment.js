8910bd22b152dfef4bd844b0afaf99e56d7b7a52
Andrzej Dąbrowski <andrzej@ratio.team>
2018-12-07 11:55:33 +0100

    added comments

--- src/components/touchSpace/TouchSpaceExperiment.js
+++ src/components/touchSpace/TouchSpaceExperiment.js
@@ -597,158 +597,158 @@ class TouchSpaceExperiment {
          * PINCH
          */
 
-        let pinchStartScale; // we need this, because sometimes hammer gives initial scale not 1, but sth like 4 or 5, god knows why
-
-        this._mc.on('pinch pinchstart pinchend pinchin pinchout', (ev) => {
-
-            if (this._blockPinchEvents) {
-                return;
-            }
-
-            console.log('pinch', ev.type);
-
-            switch (ev.type) {
-                case 'pinchstart':
-                    break;
-                case 'pinchin':
-                case 'pinchout':
-                case 'pinchmove':
-                    if (SESSION === 'pinch') {
-                        this._zoomer.pinchmove({
-                            x: ev.deltaX,
-                            y: ev.deltaY,
-                            scale: ev.scale / pinchStartScale
-                        });
-                    }
-                    else {
-                        SESSION = 'pinch';
-
-                        let clientRect = this._touchSpace.getBoundingClientRect();
-
-                        this._zoomer.pinchstart({
-                            x: ev.center.x - clientRect.left,
-                            y: ev.center.y - clientRect.top
-                        });
-
-                        pinchStartScale = ev.scale;
-                    }
-                    break;
-                case 'pinchend':
-                case 'pinchcancel':
-                    this._zoomer.pinchend();
-                    SESSION = null;
-                    break;
-                default:
-                    break;
-            }
-        });
+        // let pinchStartScale; // we need this, because sometimes hammer gives initial scale not 1, but sth like 4 or 5, god knows why
+        //
+        // this._mc.on('pinch pinchstart pinchend pinchin pinchout', (ev) => {
+        //
+        //     if (this._blockPinchEvents) {
+        //         return;
+        //     }
+        //
+        //     console.log('pinch', ev.type);
+        //
+        //     switch (ev.type) {
+        //         case 'pinchstart':
+        //             break;
+        //         case 'pinchin':
+        //         case 'pinchout':
+        //         case 'pinchmove':
+        //             if (SESSION === 'pinch') {
+        //                 this._zoomer.pinchmove({
+        //                     x: ev.deltaX,
+        //                     y: ev.deltaY,
+        //                     scale: ev.scale / pinchStartScale
+        //                 });
+        //             }
+        //             else {
+        //                 SESSION = 'pinch';
+        //
+        //                 let clientRect = this._touchSpace.getBoundingClientRect();
+        //
+        //                 this._zoomer.pinchstart({
+        //                     x: ev.center.x - clientRect.left,
+        //                     y: ev.center.y - clientRect.top
+        //                 });
+        //
+        //                 pinchStartScale = ev.scale;
+        //             }
+        //             break;
+        //         case 'pinchend':
+        //         case 'pinchcancel':
+        //             this._zoomer.pinchend();
+        //             SESSION = null;
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // });
 
 
         /**
          * PAN
          */
-
-        let stopPropagationCallback = (ev) => {
-            ev.preventDefault();
-            ev.stopPropagation();
-        };
-
-        let panPreviousCenter;
-
-        this._mc.on('panup pandown panleft panright panstart panend pancancel', (ev) => {
-
-            console.log('PAN EVENT', ev.type, SESSION);
-
-            // Prevents weird Chrome bug (Android chrome too) with incorrect pan events showing up.
-            // https://github.com/hammerjs/hammer.js/issues/1050
-            // if (ev.srcEvent.type == "pointercancel") {
-            //     return;
-            // }
-
-            switch (ev.type) {
-                case "panstart":
-                    if (this._blockPanEvents) {
-                        break;
-                    }
-                    break;
-
-                case "panup":
-                case "pandown":
-                case "panleft":
-                case "panright":
-                    if (this._blockPanEvents) {
-                        break;
-                    }
-
-                    if (SESSION === 'pan-swiper') {
-                        this._touchSpaceController.panMove(ev.deltaX);
-                    }
-                    else if (SESSION === 'pan-zoomer') {
-
-                        let panNewCenter = ev.center;
-
-                        this._zoomer.moveTo({
-                            x: this._zoomer.pos.x + (panNewCenter.x - panPreviousCenter.x),
-                            y: this._zoomer.pos.y + (panNewCenter.y - panPreviousCenter.y),
-                            scale: this._zoomer.pos.scale
-                        });
-
-                        panPreviousCenter = panNewCenter;
-                    }
-                    // Start new session!
-                    else if (SESSION === null) {
-
-                        if (
-                            this._zoomer.pos.scale > 1 &&
-                            !(ev.type === 'panleft' && this._zoomer.isAlignedToRight()) &&
-                            !(ev.type === 'panright' && this._zoomer.isAlignedToLeft())
-                        ) {
-                            SESSION = 'pan-zoomer';
-
-                            panPreviousCenter = ev.center;
-                        }
-                        else {
-
-                            if (Math.abs(ev.deltaY) > Math.abs(ev.deltaX)) {
-                                // console.log('MC stop!!!');
-                                // this._mc.stop(true); // no more events until new session
-                                return;
-                            }
-
-                            SESSION = 'pan-swiper';
-                            this._touchSpace.addEventListener('click', stopPropagationCallback, true);
-                            this._touchSpaceController.panStart();
-                        }
-                    }
-                    break;
-
-                case "panend":
-                case "pancancel":
-                    if (this._blockPanEvents) {
-                        break;
-                    }
-
-                    // this._blockPanEvents = false;
-
-                    if (SESSION === 'pan-swiper') {
-                        setTimeout(() => {
-                            touchSpace.removeEventListener('click', stopPropagationCallback, true);
-                        }, 0);
-
-                        this._touchSpaceController.panEnd(-ev.velocityX);
-                    }
-                    else if (SESSION === 'pan-zoomer') {
-                        this._zoomer.snap(true);
-                    }
-
-                    SESSION = null;
-                    break;
-
-                default:
-                    break;
-            }
-
-        });
+        //
+        // let stopPropagationCallback = (ev) => {
+        //     ev.preventDefault();
+        //     ev.stopPropagation();
+        // };
+        //
+        // let panPreviousCenter;
+        //
+        // this._mc.on('panup pandown panleft panright panstart panend pancancel', (ev) => {
+        //
+        //     console.log('PAN EVENT', ev.type, SESSION);
+        //
+        //     // Prevents weird Chrome bug (Android chrome too) with incorrect pan events showing up.
+        //     // https://github.com/hammerjs/hammer.js/issues/1050
+        //     // if (ev.srcEvent.type == "pointercancel") {
+        //     //     return;
+        //     // }
+        //
+        //     switch (ev.type) {
+        //         case "panstart":
+        //             if (this._blockPanEvents) {
+        //                 break;
+        //             }
+        //             break;
+        //
+        //         case "panup":
+        //         case "pandown":
+        //         case "panleft":
+        //         case "panright":
+        //             if (this._blockPanEvents) {
+        //                 break;
+        //             }
+        //
+        //             if (SESSION === 'pan-swiper') {
+        //                 this._touchSpaceController.panMove(ev.deltaX);
+        //             }
+        //             else if (SESSION === 'pan-zoomer') {
+        //
+        //                 let panNewCenter = ev.center;
+        //
+        //                 this._zoomer.moveTo({
+        //                     x: this._zoomer.pos.x + (panNewCenter.x - panPreviousCenter.x),
+        //                     y: this._zoomer.pos.y + (panNewCenter.y - panPreviousCenter.y),
+        //                     scale: this._zoomer.pos.scale
+        //                 });
+        //
+        //                 panPreviousCenter = panNewCenter;
+        //             }
+        //             // Start new session!
+        //             else if (SESSION === null) {
+        //
+        //                 if (
+        //                     this._zoomer.pos.scale > 1 &&
+        //                     !(ev.type === 'panleft' && this._zoomer.isAlignedToRight()) &&
+        //                     !(ev.type === 'panright' && this._zoomer.isAlignedToLeft())
+        //                 ) {
+        //                     SESSION = 'pan-zoomer';
+        //
+        //                     panPreviousCenter = ev.center;
+        //                 }
+        //                 else {
+        //
+        //                     if (Math.abs(ev.deltaY) > Math.abs(ev.deltaX)) {
+        //                         // console.log('MC stop!!!');
+        //                         // this._mc.stop(true); // no more events until new session
+        //                         return;
+        //                     }
+        //
+        //                     SESSION = 'pan-swiper';
+        //                     this._touchSpace.addEventListener('click', stopPropagationCallback, true);
+        //                     this._touchSpaceController.panStart();
+        //                 }
+        //             }
+        //             break;
+        //
+        //         case "panend":
+        //         case "pancancel":
+        //             if (this._blockPanEvents) {
+        //                 break;
+        //             }
+        //
+        //             // this._blockPanEvents = false;
+        //
+        //             if (SESSION === 'pan-swiper') {
+        //                 setTimeout(() => {
+        //                     touchSpace.removeEventListener('click', stopPropagationCallback, true);
+        //                 }, 0);
+        //
+        //                 this._touchSpaceController.panEnd(-ev.velocityX);
+        //             }
+        //             else if (SESSION === 'pan-zoomer') {
+        //                 this._zoomer.snap(true);
+        //             }
+        //
+        //             SESSION = null;
+        //             break;
+        //
+        //         default:
+        //             break;
+        //     }
+        //
+        // });
     }
 
     disable() {
